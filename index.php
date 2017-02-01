78a1ecf734a8f516a9c326d161bf9e6dfd3d6350
ilevd <ilevd@mail.ru>
2017-02-01 13:53:01 +0700

    Format index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>AnyChart PHP template</title>
     <script src="https://cdn.anychart.com/js/latest/anychart-bundle.min.js"></script>
-	<script src="http://cdn.anychart.com/js/latest/data-adapter.min.js"></script>
+    <script src="http://cdn.anychart.com/js/latest/data-adapter.min.js"></script>
     <link rel="stylesheet" href="https://cdn.anychart.com/css/latest/anychart-ui.min.css"/>
     <link rel="stylesheet" href="static/css/style.css"/>
 </head>
@@ -12,12 +12,12 @@
 <div id="container"></div>
 <script>
     anychart.onDocumentReady(function() {
-		anychart.data.loadJsonFile("/data.php", function (data) {
-		    var chart = anychart.pie(data);
-		    chart.title("Top 5 fruits");
-		    chart.container("container");
+        anychart.data.loadJsonFile("/data.php", function (data) {
+            var chart = anychart.pie(data);
+            chart.title("Top 5 fruits");
+            chart.container("container");
 		    chart.draw();
-		});
+        });
     });
 </script>
 </body>
